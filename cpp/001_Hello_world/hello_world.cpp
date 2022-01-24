ba7cc6551fb49c44a827e3b5f438275b6256eb27
amserafini <aseraf@bu.edu>
2022-01-24 18:41:23 +0100

    'first try'

--- cpp/001_Hello_world/hello_world.cpp
+++ cpp/001_Hello_world/hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-  std::string firstName;
+  std::string firstName; 
   std::cout << "Type your first name: ";
   std::cin >> firstName;
   std::cout << "Hello world, hello " << firstName << std::endl;
