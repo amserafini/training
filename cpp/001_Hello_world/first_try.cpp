5796b9845e97380d5cc2d3337b930c786a1ee43c
amserafini <aseraf@bu.edu>
2022-01-25 14:44:17 +0100

    deleting demo file

--- cpp/001_Hello_world/first_try.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> //so that Cout works
-#include <string> //in order to store text, for example, "hello"
-
-int main () {
-    std::string firstName;
-    std::cout << "Type your first name:";
-    std::cin >> firstName;
-    std::cout << "Hello world, hello " << firstName << std::endl;
-} //oh
