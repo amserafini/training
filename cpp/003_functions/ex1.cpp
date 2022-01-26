7f97ba3168f6d58ac31706cf0c45c4bf97b0c26b
amserafini <aseraf@bu.edu>
2022-01-26 15:21:03 +0100

    Exercise 1

--- cpp/003_functions/ex1.cpp
+++ cpp/003_functions/ex1.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 
-//define your functions here
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
 
-int main(int argc, char** argv) {
-  //write your code here
+    std::cout << "Enter the number of terms: ";
+    std::cin >> n;
 
-  return 0;
+    std::cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        // Prints the first two terms.
+        if(i == 1) {
+            std::cout << t1 << ", ";
+            continue;
+        }
+        if(i == 2) {
+            std::cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        std::cout << nextTerm << ", ";
+    }
+    return 0;
 }
